23ce07ff832bcc057c42230cca30a44844482668
MatthewDilks <19dilksm@roundwoodpark.co.uk>
2025-03-10 12:00:22 +0000

    Update Control.cs

--- uis/Control.cs
+++ uis/Control.cs
@@ -3,13 +3,23 @@ using System;
 
 public partial class Control : Godot.Control
 {
-	// Called when the node enters the scene tree for the first time.
+	PackedScene GameManager;
+	Node CurrentGame;
+ 	ManagerScript CurrentGameScript;
+ 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+ 		GameManager = GD.Load<PackedScene>("snake/res://Scenes/manager_scene.tcsn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+ 	void initNewGame() {
+		CurrentGame = GameManager.Instantiate();
+  		addChild(currentGame);
+    		CurrentGameScript = CurrentGame as ManagerScript;
+    	}
+  	
 }
